1e65b37d8009ac548fbd4cff0f6b96b819b67dab
heifetzwu <heifetzwu@gmail.com>
2018-02-01 07:05:49 +0000

    mod index.js to inc github-flow

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 
 var app = express()
 app.get('/',function(req,res){
-  res.send('Hello World')
+  res.send('Hello World from github-flow')
 })
 var server = app.listen(8081,function(){
 var port = server.address().port
