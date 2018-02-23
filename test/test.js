03fe1eab1d55700bb4ef2c0df9a6e2a256c4ef2c
heifetzwu <heifetzwu@gmail.com>
2018-02-23 16:01:07 +0000

    remove #

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ describe('Array',function(){
   describe('#indexOf()',function(){
     it('Return -1 if element can not be searched',function(){
       assert.equal(-1,[1,2,3].indexOf(0))
-      # should be right
     })
   })
 })
